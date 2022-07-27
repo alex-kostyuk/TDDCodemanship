36e288d271e93594fc3822fff69f5f67608c685b
Alex Kostyuk <alexander.kostyuk@wisetechglobal.com>
2022-07-27 09:41:57 +0200

    add MarsRover project

---
+++ MarsRover/MarsRover.Tests/RoverTest.cs
@@ -0,0 +1,136 @@
+﻿using Model;
+using NUnit.Framework;
+
+namespace TDDTesting
+{
+	internal class RoverTest
+	{
+		[Test]
+		public void TestMoveForwardOnce()
+		{
+			Rover rover = new Rover((0, 0), 'N');
+			rover.Run("F");
+			Assert.That(rover.Location, Is.EqualTo((0, 1)));
+		}
+
+		[Test]
+		public void TestMoveForwardTwice()
+		{
+			Rover rover = new Rover((0, 0), 'N');
+			rover.Run("FF");
+			Assert.That(rover.Location, Is.EqualTo((0, 2)));
+		}
+
+		[Test]
+		public void TestMoveBackwardOnce()
+		{
+			Rover rover = new Rover((0,0), 'N');
+			rover.Run("B");
+			Assert.That(rover.Location, Is.EqualTo((0, -1)));
+		}
+
+		[Test]
+		public void TestTurnRightOnce()
+		{
+			Rover rover = new Rover((0, 0), 'N');
+			rover.Run("R");
+			Assert.That(rover.Direction, Is.EqualTo('E'));
+		}
+
+		[Test]
+		public void TestTurnRightTwice()
+		{
+			Rover rover = new Rover((0, 0), 'N');
+			rover.Run("RR");
+			Assert.That(rover.Direction, Is.EqualTo('S'));
+		}
+
+		[Test]
+		public void TestTurnLeftOnce()
+		{
+			Rover rover = new Rover((0, 0), 'N');
+			rover.Run("L");
+			Assert.That(rover.Direction, Is.EqualTo('W'));
+		}
+
+		[Test]
+		public void TestTurnLeftTwice()
+		{
+			Rover rover = new Rover((0, 0), 'N');
+			rover.Run("LL");
+			Assert.That(rover.Direction, Is.EqualTo('S'));
+		}
+
+		[Test]
+		public void TestTurnLeftTurnRight()
+		{
+			Rover rover = new Rover((0, 0), 'N');
+			rover.Run("RL");
+			Assert.That(rover.Direction, Is.EqualTo('N'));
+		}
+
+		[Test]
+		public void TestTurnRightMoveForward()
+		{
+			Rover rover = new Rover((0, 0), 'N');
+			rover.Run("RF");
+			Assert.That(rover.Location, Is.EqualTo((1, 0)));
+		}
+
+		[Test]
+		public void TestTurnLeftMoveForward()
+		{
+			Rover rover = new Rover((0, 0), 'N');
+			rover.Run("LF");
+			Assert.That(rover.Location, Is.EqualTo((-1, 0)));
+		}
+
+		[Test]
+		public void TestMoveForwardStartingSouth()
+		{
+			Rover rover = new Rover((0, 0), 'S');
+			rover.Run("F");
+			Assert.That(rover.Location, Is.EqualTo((0, -1)));
+		}
+
+		[Test]
+		public void TestMoveBackwardStartingEast()
+		{
+			Rover rover = new Rover((0, 0), 'E');
+			rover.Run("B");
+			Assert.That(rover.Location, Is.EqualTo((-1, 0)));
+		}
+
+		[Test]
+		public void TestMoveBackwardStartingSouth()
+		{
+			Rover rover = new Rover((0, 0), 'S');
+			rover.Run("B");
+			Assert.That(rover.Location, Is.EqualTo((0, 1)));
+		}
+
+		[Test]
+		public void TestMoveBackwardStartingWest()
+		{
+			Rover rover = new Rover((0, 0), 'W');
+			rover.Run("B");
+			Assert.That(rover.Location, Is.EqualTo((1, 0)));
+		}
+
+		[Test]
+		public void TestSequenceOfInstruction()
+		{
+			Rover rover = new Rover((3, 5), 'S');
+			rover.Run("LFFRBB");
+			Assert.That(rover.Location, Is.EqualTo((5, 7)));
+		}
+
+        [Test]
+		public void TestTurnRighFromWest()
+        {
+			Rover rover = new Rover((0, 0), 'W');
+			rover.Run("R");
+			Assert.That(rover.Direction, Is.EqualTo('N'));
+		}
+	}
+}
---
+++ MarsRover/MarsRover/Rover.cs
@@ -0,0 +1,67 @@
+﻿namespace Model
+{
+	public class Rover
+	{
+		private string directions = "NESW";
+		public char Direction { get; private set; }
+
+		public (int x, int y) Location { get; private set; }
+
+		public Rover((int, int) location, char direction)
+		{
+			this.Location = location;
+			this.Direction = direction;
+		}
+
+		public void Run(string command)
+		{
+            char currentDirection = Direction;
+			int pathY = 0;
+			int pathX = 0;
+			foreach (var step in command)
+            {
+                DetectLocationChanges(ref pathY, ref pathX, step, currentDirection);
+                currentDirection = ChangeDirection(step, currentDirection);
+            }
+            Direction = currentDirection;
+            Location = (Location.x + pathX, Location.y + pathY);
+		}
+
+        private char ChangeDirection(char step, char direction)
+        {
+            char result = direction;
+            var currentIndex = directions.IndexOf(direction);
+            if (step == 'R')
+            {
+                result = directions[currentIndex == 3 ? 0 : currentIndex + 1];
+            }
+            if (step == 'L')
+            {
+                result = directions[currentIndex == 0 ? 3 : currentIndex - 1];
+            }
+            return result;
+        }
+
+        private void DetectLocationChanges(ref int pathY, ref int pathX, char step, char direction)
+        {
+            if (step == 'F' || step == 'B')
+            {
+                switch (direction)
+                {
+                    case 'N':
+                        pathY += step == 'F' ? 1 : -1;
+                        break;
+                    case 'E':
+                        pathX += step == 'F' ? 1 : -1;
+                        break;
+                    case 'S':
+                        pathY += step == 'F' ? -1 : 1;
+                        break;
+                    case 'W':
+                        pathX += step == 'F' ? -1 : 1;
+                        break;
+                }
+            }
+        }
+    }
+}
